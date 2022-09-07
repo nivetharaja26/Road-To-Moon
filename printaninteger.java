21d0da609c0497872a743af5ba3fcdbfffb32df5
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-09-07 07:00:51 +0530

    Print an integer - Day1

---
+++ printaninteger.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter a number:");
+        Scanner s=new Scanner(System.in);
+        int input=s.nextInt();
+        System.out.println("You entered : "+input);
+    }
+}
