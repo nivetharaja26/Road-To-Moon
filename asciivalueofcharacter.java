0414719e858f76a0c456767e94619f10252a6961
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-09-07 07:20:42 +0530

    Ascii value of a character - Day 1

---
+++ asciivalueofcharacter.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) throws Exception{
+    char alp='a';
+    int ascii_of_alp=alp;
+    System.out.println("The ascii value is : "+ascii_of_alp);
+    System.out.println("The ascii value is : "+ (int)alp );
+    }
+}
