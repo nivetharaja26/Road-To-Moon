9d58c0119bf93cf4e311a1b6ab66997e2603be3b
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-09-07 07:23:44 +0530

    quotient and reminder of integers

---
+++ quo_and_rem.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) throws Exception{
+    
+    int d=5;
+    int quotient=d/6;
+    int reminder=d%6;
+    System.out.println("The quotient is "+quotient+"and the reminder is "+reminder);
+    
+    
+    }
+}
