1929e5985320458965ed64b782e530dfbc93eafa
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-09-08 19:09:56 +0530

    Swap with temporary variable

---
+++ 01_SwapWithTemp.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int a=5,b=7;
+        int c;
+        System.out.print("before swaping");
+        System.out.print("a="+a);
+        System.out.print("b="+b);
+        
+        c=a;
+        a=b;
+        b=c;
+        System.out.print("after swaping");
+        System.out.print("a="+a);
+        System.out.print("b="+b);
+        
+    }
+}
