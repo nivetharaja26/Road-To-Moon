d59ef5c5a963e8694de1f153bf84893196049198
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-09-15 09:19:56 +0530

    printing alphabets from a to z

---
+++ 18 AlphabetsPrint.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Alphabets are : ");
+        
+    char c;
+
+    for(c = 'A'; c <= 'Z';++c)
+      System.out.print(c + " ");
+    }
+}
