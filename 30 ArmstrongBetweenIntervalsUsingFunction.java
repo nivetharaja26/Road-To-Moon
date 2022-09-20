e5d193b0db57167c8b5ad5383a5bf7785844ba03
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-09-20 08:02:11 +0530

    Armstrong number between intervals using function

---
+++ 30 ArmstrongBetweenIntervalsUsingFunction.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+       ARM();
+    }
+    public static void ARM()
+    {
+         System.out.println("Enter a low range : ");
+        Scanner s=new Scanner(System.in);
+        int p=s.nextInt();
+        System.out.println("Enter a high range : "); 
+      
+         int q=s.nextInt();int copy,res,inter;
+     for(int i=p;i<q;i++)    {
+         copy=i;
+         res=0;
+        while(copy>0){
+            inter=copy%10;
+            res=res+inter*inter*inter;
+            copy=copy/10;
+        }
+        if(res==i)
+        {
+            System.out.print(res+" ");
+            
+        }
+    }
+}}
