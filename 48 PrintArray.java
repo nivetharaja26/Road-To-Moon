73bce4c1fddffed3ec058c41664572cefd130720
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-10-12 18:36:16 +0530

    Create 48 PrintArray.java

---
+++ 48 PrintArray.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        int[] myarray = {1, 2, 3, 4, 5};
+        for (int e: myarray) {
+            System.out.println(e);
+        }
+    }
+}
