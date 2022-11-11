5e625e80ad487b21f7cb63d88cde589aa19a3bf0
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-11-11 20:32:41 +0530

    check empty string

---
+++ 60 CheckEmptyString.java
@@ -0,0 +1,12 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String mystr="    ",mystr2=null,mystr3="";
+        System.out.println(mystr.trim().isEmpty());
+        System.out.println(mystr3.isEmpty());
+        if(mystr2==null)
+        System.out.println("true");
+        
+        
+    }
+}
