f0e3cef47bb502d8a437682cb1808ab2ed072109
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-11-14 19:29:40 +0530

    Remove prime numbers from array

--- 61 RemovePrimeNumbersFromArray.java
+++ 61 RemovePrimeNumbersFromArray.java
@@ -1,26 +1,62 @@
-
+import java.io.*;
 import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
-		String input;
-		
-		Scanner s=new Scanner(System.in);
-		
-		input=s.nextLine();
-		
-		System.out.print(input);
-		int len=input.length();
-		for(int i=0;i<len;i++)
-		{
-		    if(i==0)
-		    
-		   for(int j=i;j<len-;j++)
-		   {
-		       System.out.print(input.charAt(j));
-		   }
-		}
-	}
+class Main {
+    public static void main(String[] args) throws Exception {
+        
+      
+        System.out.print("Enter n:");
+        
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        
+        
+        int[] a=new int[n];
+        int[] b=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        
+       
+        int j,flag,temp,l=0;
+        for(int i=0;i<n;i++)
+        {
+            flag=0;
+            
+            for(j=2;j<a[i];j++)
+            {
+                if(a[i]%j==0)
+                {
+                    flag=1;
+                    break;
+                }
+                
+            }
+            /*
+            if(flag==1 && a[i]!=2)
+            {
+                int k=i;
+                while(k<n-1)
+                {
+                temp=a[k];
+                a[k]=a[k+1];
+                k++;
+                }
+                n--;
+            }
+            **/
+            
+            if(flag==1)
+            {
+                b[l]=a[i];
+                l++;
+                
+            }
+        }
+         for(int i=0;i<l;i++)
+        {
+            System.out.print(b[i]+" ");
+        }
+        
+    }
 }
