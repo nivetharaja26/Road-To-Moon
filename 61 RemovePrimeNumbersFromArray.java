6ead576eb1bdc59a7f2527656023055b38263203
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-11-14 19:01:09 +0530

    Remove prime numbers from array

---
+++ 61 RemovePrimeNumbersFromArray.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		String input;
+		
+		Scanner s=new Scanner(System.in);
+		
+		input=s.nextLine();
+		
+		System.out.print(input);
+		int len=input.length();
+		for(int i=0;i<len;i++)
+		{
+		    if(i==0)
+		    
+		   for(int j=i;j<len-;j++)
+		   {
+		       System.out.print(input.charAt(j));
+		   }
+		}
+	}
+}
